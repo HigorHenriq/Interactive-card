c842510078fc70c56b96b4383594bc19f546d90d
higorhenquiq <higor.henrique2701@gmail.com>
2022-11-15 18:52:23 -0300

    :construction: IC-03 adicionando interatividade do card com input e novos metodos para validacao

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.querySelector("form");
+
+const cardClientName = document.getElementById("cardClientName");
+
+const resetCardName = () => {
+	cardClientName.innerHTML = "CLIENT NAME HERE!";
+};
+
+export const changeCardName = (event) => {
+	if (event == "") {
+		return resetCardName();
+	}
+
+	cardClientName.innerHTML = event;
+};
+
+/**
+ * @modules
+ */
+
+/**
+ * Regex String for special caracters validation, except space
+ */
+const regxStr = "/[^a-zA-Z 0-9]+/g";
+
+function hasAnySpecialCrt(str) {
+	let regx = new RegExp(regxStr);
+
+	if (regx.test(str)) {
+		return true;
+	}
+
+	return false;
+}
+
+function removeSpecialCrt(str) {
+	str = str.replace(regxStr, "");
+
+	return str;
+}
